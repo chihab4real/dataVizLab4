af516e3da7627f10f9c139cfc24caa1eeb268a88
Wikusia-s <wik.szarzynska@gmail.com>
2024-05-21 19:40:52 +0200

    Create help/about section

--- server.R
+++ server.R
@@ -1,6 +1,21 @@
 library(shiny)
 library(shinydashboard)
+library(shinyjs)
 
-shinyServer(function(input, output){
+server <- function(input, output, session) {
+  observeEvent(input$q1, {
+    toggle("a1")
+  })
   
-})
+  observeEvent(input$q2, {
+    toggle("a2")
+  })
+  
+  observeEvent(input$q3, {
+    toggle("a3")
+  })
+  
+  observeEvent(input$q4, {
+    toggle("a4")
+  })
+}
--- ui.R
+++ ui.R
@@ -1,7 +1,9 @@
 library(shiny)
 library(shinydashboard)
 library(fresh)
+library(shinyjs)
 
+# my own theme using fresh
 my_theme <- create_theme(
   adminlte_color(
     light_blue = "#C8102E"  
@@ -21,32 +23,72 @@ my_theme <- create_theme(
 
 shinyUI(
   dashboardPage(
-    dashboardHeader(title = "NBA"),
-    dashboardSidebar(
-      sidebarMenu(
-        menuItem(HTML('<i class="fas fa-home"></i> Home'), tabName = "home"),      
-        menuItem(HTML('<i class="fas fa-history"></i> History'), tabName = "history"),      
-        menuItem(HTML('<i class="fas fa-users"></i> Team'), tabName = "team"),
-        menuItem(HTML('<i class="fas fa-basketball-ball"></i> Players'), tabName = "players")      
-    )),
-    dashboardBody(
-      use_theme(my_theme),
-      tabItems(
-        tabItem(tabName = "home", 
-                h1("Home dashboard")
-                ),
-        tabItem(tabName = "history",
-                h1("History dashboard")
-                ),
-        tabItem(tabName = "team",
-                h1("Team dashboard")
-                ),
-        tabItem(tabName = "players",
-                h1("Players dashboard")
+  dashboardHeader(title = "NBA"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem(HTML('<i class="fas fa-home"></i> Home'), tabName = "home"),
+      menuItem(HTML('<i class="fas fa-history"></i> History'), tabName = "history"),
+      menuItem(HTML('<i class="fas fa-users"></i> Team'), tabName = "team"),
+      menuItem(HTML('<i class="fas fa-basketball-ball"></i> Players'), tabName = "players"),
+      menuItem(HTML('<i class="fa-solid fa-question"></i> Help'), tabName = "qa")
+    )
+  ),
+  dashboardBody(
+    use_theme(my_theme),
+    useShinyjs(),  # Użyj shinyjs
+    tabItems(
+      tabItem(tabName = "home",
+              h1("Home dashboard")
+      ),
+      tabItem(tabName = "history",
+              h1("History dashboard")
+      ),
+      tabItem(tabName = "team",
+              h1("Team dashboard")
+      ),
+      tabItem(tabName = "players",
+              h1("Players dashboard")
+      ),
+      tabItem(tabName = "qa",
+              h2("Questions and Answers"),
+              div(
+                style = "margin-bottom: 20px;",  # Dodaj trochę miejsca między pytaniami
+                actionButton("q1", "What is this application about?"),
+                hidden(
+                  div(id = "a1",
+                      p("This application provides various information about NBA teams and players, including historical data and player statistics.")
+                  )
                 )
-        )
+              ),
+              div(
+                style = "margin-bottom: 20px;",
+                actionButton("q2", "What kind of information can I get from this app?"),
+                hidden(
+                  div(id = "a2",
+                      p("You can get information about NBA teams, player statistics, historical data, and more.")
+                  )
+                )
+              ),
+              div(
+                style = "margin-bottom: 20px;",
+                actionButton("q3", "How to navigate the tabs to explore different sections?"),
+                hidden(
+                  div(id = "a3",
+                      p("Use the sidebar menu to navigate between different sections: Home, History, Team, Players, and Help.")
+                  )
+                )
+              ),
+              div(
+                style = "margin-bottom: 20px;",
+                actionButton("q4", "How can I contact you?"),
+                hidden(
+                  div(id = "a4",
+                      p("You can contact us via email at support@example.com.")
+                  )
+                )
+              )
       )
     )
+  )
 )
-
-
+)
