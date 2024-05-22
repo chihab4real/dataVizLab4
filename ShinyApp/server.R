f15f32147453ad4899391efe303ceee9647dbaa8
Wiktoria <150298072+Wikusia-s@users.noreply.github.com>
2024-05-22 20:38:51 +0200

    tidy up

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -1,8 +1,32 @@
 library(shiny)
 library(shinydashboard)
 library(shinyjs)
-
+library(dplyr)
+library(shinythemes)
 server <- function(input, output, session) {
+  team <- read.csv("csv files/team.csv", header = TRUE, stringsAsFactors = FALSE)
+  team <- team %>% select(-id) 
+  colnames(team) <- c("Team Name", "Abbreviation", "Nickname", "City", "State", "Year Established")
+
+  output$team <- DT::renderDataTable({
+    DT::datatable(team, 
+                  style = "bootstrap",
+                  options = list(pageLength = 10,
+                                 lengthMenu = c(5,10,20),
+                                 searching = TRUE,
+                                 ordering = TRUE,
+                                 dom = 'Bfrtip',
+                                 buttons = list(
+                                   list(extend = 'copy', text = '<i class="fa fa-copy"></i>'),
+                                   list(extend = 'csv', text = '<i class="fa fa-file-csv"></i>'),
+                                   list(extend = 'excel', text = '<i class="fa fa-file-excel"></i>')
+                                   ),
+                                 class = 'stripe hover'),
+                  extensions = c('Buttons')
+                  )
+  })
+  
+  
   observeEvent(input$q1, {
     toggle("a1")
   })
@@ -18,4 +42,5 @@ server <- function(input, output, session) {
   observeEvent(input$q4, {
     toggle("a4")
   })
+
 }
